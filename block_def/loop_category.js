e22ca7d077237192ca18d4ee543d833dae6bd953
KazumaOmura <kazumaxbaseball123@gmail.com>
2022-03-10 05:22:20 +0900

    条件付きloopブロック追加

---
+++ block_def/loop_category.js
@@ -0,0 +1,230 @@
+// 比較演算子
+Blockly.Blocks.arg_compare = {
+    init() {
+      this.jsonInit({
+        type: "block_type",
+        message0: "%1 %2 %3 %4",
+        args0: [{
+          type: "input_value",
+          name: "left_side",
+        },
+        {
+          type: "field_dropdown",
+          name: "operator",
+          options: [
+            [
+              "=",
+              "=",
+            ],
+            [
+              "<",
+              "<"
+            ],
+            [
+              ">",
+              ">"
+            ],
+            [
+              "<=",
+              "<="
+            ],
+            [
+              ">=",
+              ">="
+            ]
+          ]
+        },
+        {
+          type: "input_dummy"
+        },
+        {
+          type: "input_value",
+          name: "right_side",
+        }
+        ],
+        output: null,
+        colour: 150,
+        tooltip: "",
+        helpUrl: ""
+      });
+    },
+  };
+  
+  Blockly.Hat.arg_compare = function (block) {
+    const left_side = Blockly.Hat.valueToCode(block, 'left_side', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const operator = block.getFieldValue('operator');
+    const right_side = Blockly.Hat.valueToCode(block, 'right_side', Blockly.Hat.ORDER_FUNCTION_CALL);
+  
+    return left_side + " " + operator + " " + right_side;
+  
+  };
+  
+
+//繰り返し
+Blockly.Blocks.loop = {
+    init() {
+        this.jsonInit({
+            type: "for",
+            message0: "%1 回繰り返す %2 do %3",
+            args0: [{
+                    type: "field_input",
+                    name: "num",
+                    text: "10",
+                    check: "Num"
+                },
+                {
+                    type: "input_dummy"
+                },
+                {
+                    type: "input_statement",
+                    name: "do"
+                }
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            colour: 150,
+            tooltip: "",
+            helpUrl: ""
+        });
+    },
+};
+Blockly.Hat.loop = function (block) {
+    const num = block.getFieldValue('num');
+    const do_order = Blockly.Hat.statementToCode(block, 'do', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const OPERATOR = "fix";
+    const OPERATOR2 = "(^(loop i . break)";
+    const OPERATOR3 = "i + 1 ^(i+1)";
+    const break_order_left = "if(i+1 > ";
+    const break_order_right = ") break"; 
+    const OPERATOR4 = "loop i+1 . break";
+    
+    return OPERATOR + "\n" + OPERATOR2 + "\n" + OPERATOR3 + "\n" + break_order_left + num + break_order_right + "\n" + "(" + do_order + "^()" + "\n" + OPERATOR4 + "\n" + ")" + "\n" + ") 0";
+};
+
+
+// 条件付きloop
+Blockly.Blocks.condition_loop = {
+    init() {
+        this.jsonInit({
+            type: "controls_if",
+            // message0: "if %1 %2 do %3",
+            message0: "loop関数 %1 引数 %2 %3 break条件 %4 do %5 return %6",
+            args0: [{
+                type: "input_dummy"
+                },
+                {
+                type: "input_statement",
+                name: "arg",
+                },
+                {
+                    type: "input_dummy"
+                },
+                {
+                    type: "input_value",
+                    name: "if_conditions",
+                },
+                {
+                    type: "input_statement",
+                    name: "do",
+                },
+                {
+                    type: "input_statement",
+                    name: "return",
+                }
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            // output: null,
+            colour: 150,
+            tooltip: "",
+            helpUrl: "",
+        });
+    },
+};
+
+Blockly.Hat.condition_loop = function (block) {
+    const arg = Blockly.Hat.statementToCode(block, 'arg', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const if_conditions = Blockly.Hat.statementToCode(block, 'if_conditions', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const do_order = Blockly.Hat.statementToCode(block, 'do', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const return_code = Blockly.Hat.statementToCode(block, 'return', Blockly.Hat.ORDER_FUNCTION_CALL);
+    const OPERATOR = "fix(^(loop ";
+    const OPERATOR2 = " . break)";
+    const break_order_left = "if(";
+    const break_order_right = ") break"; 
+    const OPERATOR3 = "loop ";
+    const OPERATOR4 = " . break";
+
+    // 数値の長さを判定し、配列に分割した後にHat形式で出力
+    const nums = arg.split('.');
+    const nums_length = nums.length
+
+    // 入力された数値の整列
+    let temporary_num = '';
+    let n = 0;
+    while (n <= nums_length - 2) { // 配列の最後のメモリが空白になるので-1で調整
+        temporary_num += nums[n] + " ";
+        n++;
+    }
+    
+    return OPERATOR + temporary_num + OPERATOR2 + "\n" + break_order_left + if_conditions + break_order_right + "\n" + "(" + "\n" + do_order + "\n" + OPERATOR3 + return_code + OPERATOR4 + "\n" + ")" + "\n" + ") 0";
+};
+
+
+
+
+//loop&break
+Blockly.Blocks.loop_if_break = {
+    /**
+     * Block for shuffle characters.
+     * @this Blockly.Block
+     */
+    init() {
+        this.jsonInit({
+            type: "block_type",
+            message0: "繰り返し %1 %2 初期値 %3 処理 %4 脱出 %5 %6",
+            args0: [{
+                    type: "field_input",
+                    name: "value",
+                    text: "loop"
+                },
+                {
+                    type: "input_dummy"
+                },
+                {
+                    type: "input_statement",
+                    name: "arg"
+                },
+                {
+                    type: "input_statement",
+                    name: "do"
+                },
+                {
+                    type: "field_input",
+                    name: "value2",
+                    text: "break"
+                },
+                {
+                    type: "input_dummy"
+                }
+            ],
+            previousStatement: null,
+            nextStatement: null,
+            colour: 150,
+            tooltip: "",
+            helpUrl: ""
+        });
+    },
+};
+Blockly.JavaScript.loop_if_break = function (block) {
+    const OPERATOR = "for(;;){";
+    const OPERATOR2_1 = "{";
+    const OPERATOR2_2 = "}";
+    const do_code = Blockly.JavaScript.valueToCode(block, 'do', Blockly.JavaScript.ORDER_FUNCTION_CALL) || '\'\'';
+    const break_code = Blockly.JavaScript.valueToCode(block, 'break', Blockly.JavaScript.ORDER_ATOMIC);
+    const OPERATOR3 = "if(";
+    return [OPERATOR + "\n" + do_code + "\n" + OPERATOR3 + break_code + ")" + OPERATOR2_1 + "\n" + "break;" + "\n" + OPERATOR2_2 + "\n" + OPERATOR2_2, Blockly.JavaScript.ORDER_MEMBER];
+};
+Blockly.Hat.loop_if_break = function (block) {
+    const order = "※未定義※";
+    return [order, Blockly.Hat.ORDER_MEMBER];
+};
--- block_def/standard_category.js
+++ block_def/standard_category.js
@@ -1,87 +1,87 @@
-  //文字列(クォーテーションあり)
-  Blockly.Blocks.string = {
-    /**
-     * Block for shuffle characters.
-     * @this Blockly.Block
-     */
-    init() {
-      this.jsonInit({
-        type: "print",
-        message0: "\" %1 \" ",
-        args0: [{
-          type: "field_input",
-          name: "string",
-          text: "123",
-        }],
-        output: null,
-        colour: 0,
-        tooltip: "",
-        helpUrl: ""
-      });
-    },
-  };
+//文字列(クォーテーションあり)
+Blockly.Blocks.string = {
+  /**
+   * Block for shuffle characters.
+   * @this Blockly.Block
+   */
+  init() {
+    this.jsonInit({
+      type: "print",
+      message0: "\" %1 \" ",
+      args0: [{
+        type: "field_input",
+        name: "string",
+        text: "abc",
+      }],
+      output: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
 
-  Blockly.Hat.string = function (block) {
-    const text_num = block.getFieldValue('string');
-    return "\"" + text_num + "\"";
-  };
+Blockly.Hat.string = function (block) {
+  const text_num = block.getFieldValue('string');
+  return "\"" + text_num + "\"";
+};
 
 
 // 数値
 Blockly.Blocks.num = {
-    /**
-     * Block for shuffle characters.
-     * @this Blockly.Block
-     */
-    init() {
-      this.jsonInit({
-        type: "block_type",
-        message0: "%1",
-        args0: [{
-          type: "field_input",
-          name: "num",
-          text: "123"
-        }],
-        output: null,
-        colour: 0,
-        tooltip: "",
-        helpUrl: ""
-      });
-    },
-  };
-  
-  Blockly.Hat.num = function (block) {
-    const num = block.getFieldValue('num');
-    return num
-  };
+  /**
+   * Block for shuffle characters.
+   * @this Blockly.Block
+   */
+  init() {
+    this.jsonInit({
+      type: "block_type",
+      message0: "%1",
+      args0: [{
+        type: "field_input",
+        name: "num",
+        text: "123"
+      }],
+      output: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
 
-    // 数値 ステートメント
-    Blockly.Blocks.num_statement = {
-      /**
-       * Block for shuffle characters.
-       * @this Blockly.Block
-       */
-      init() {
-        this.jsonInit({
-          type: "block_type",
-          message0: "%1.",
-          args0: [{
-            type: "field_input",
-            name: "num",
-            text: "123"
-          }],
-          output: null,
-          colour: 0,
-          tooltip: "",
-          helpUrl: ""
-        });
-      },
-    };
-    
-    Blockly.Hat.num_statement = function (block) {
-      const num = block.getFieldValue('num') + ".";
-      return num;
-    };
+Blockly.Hat.num = function (block) {
+  const num = block.getFieldValue('num');
+  return num
+};
+
+// 数値 ステートメント
+Blockly.Blocks.num_statement = {
+  /**
+   * Block for shuffle characters.
+   * @this Blockly.Block
+   */
+  init() {
+    this.jsonInit({
+      type: "block_type",
+      message0: "%1.",
+      args0: [{
+        type: "field_input",
+        name: "num",
+        text: "123"
+      }],
+      output: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
+
+Blockly.Hat.num_statement = function (block) {
+  const num = block.getFieldValue('num') + ".";
+  return num;
+};
 
 // 数値2
 Blockly.Blocks.num2 = {
@@ -112,173 +112,206 @@ Blockly.Hat.num2 = function (block) {
   return num
 };
 
-  // 数値2 ステートメント
-  Blockly.Blocks.num2_statement = {
-    /**
-     * Block for shuffle characters.
-     * @this Blockly.Block
-     */
-    init() {
-      this.jsonInit({
-        type: "block_type",
-        message0: "%1.",
-        args0: [{
-          type: "field_input",
-          name: "num",
-          text: "123"
-        }],
-        previousStatement: null,
-        nextStatement: null,
-        colour: 0,
-        tooltip: "",
-        helpUrl: ""
-      });
-    },
-  };
-  
-  Blockly.Hat.num2_statement = function (block) {
-    const num = block.getFieldValue('num') + ".";
-    return num;
-  };
+// 数値2 ステートメント
+Blockly.Blocks.num2_statement = {
+  /**
+   * Block for shuffle characters.
+   * @this Blockly.Block
+   */
+  init() {
+    this.jsonInit({
+      type: "block_type",
+      message0: "%1.",
+      args0: [{
+        type: "field_input",
+        name: "num",
+        text: "123"
+      }],
+      previousStatement: null,
+      nextStatement: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
+
+Blockly.Hat.num2_statement = function (block) {
+  const num = block.getFieldValue('num') + ".";
+  return num;
+};
 
 
 // 出力
 Blockly.Blocks.print = {
-    init() {
-      this.jsonInit({
-        type: "print",
-        message0: "%1 を出力 %2 改行する",
-        args0: [{
-            type: "input_value",
-            name: "arg",
-          },
-          {
-            type: "field_checkbox",
-            name: "new_line_check",
-            checked: true
-          }
-        ],
-        previousStatement: null,
-        nextStatement: null,
-        colour: 0,
-        tooltip: Blockly.Msg.TEXT_PRINT_TOOLTIP,
-        elpUrl: Blockly.Msg.TEXT_PRINT_HELPURL
-      });
-    },
-  };
-  
-  Blockly.Hat.print = function (block) {
-    if(block.getFieldValue('new_line_check') == 'TRUE'){
-      var new_line_check = " \"\\n\"";
-    }
-    else{
-      var new_line_check = "";
-    }
-    const arg = Blockly.Hat.statementToCode(block, 'arg', Blockly.Hat.ORDER_FUNCTION_CALL);
-    let OPERATOR = "print("
-    OPERATOR += arg;
-    OPERATOR += new_line_check;
-    OPERATOR += ") ^()";
-    return OPERATOR + "\n";
-  };
+  init() {
+    this.jsonInit({
+      type: "print",
+      message0: "%1 を出力 %2 改行する",
+      args0: [{
+        type: "input_value",
+        name: "arg",
+      },
+      {
+        type: "field_checkbox",
+        name: "new_line_check",
+        checked: true
+      }
+      ],
+      previousStatement: null,
+      nextStatement: null,
+      colour: 0,
+      tooltip: Blockly.Msg.TEXT_PRINT_TOOLTIP,
+      elpUrl: Blockly.Msg.TEXT_PRINT_HELPURL
+    });
+  },
+};
+
+Blockly.Hat.print = function (block) {
+  if (block.getFieldValue('new_line_check') == 'TRUE') {
+    var new_line_check = " \"\\n\"";
+  }
+  else {
+    var new_line_check = "";
+  }
+  const arg = Blockly.Hat.statementToCode(block, 'arg', Blockly.Hat.ORDER_FUNCTION_CALL);
+  let OPERATOR = "print("
+  OPERATOR += arg;
+  OPERATOR += new_line_check;
+  OPERATOR += ") ^()";
+  return OPERATOR + "\n";
+};
+
+// インクリメント演算
+Blockly.Blocks.increment_operator = {
+  init() {
+    this.jsonInit({
+      type: "block_type",
+      message0: "%1 %2 = %3",
+      args0: [{
+        type: "input_value",
+        name: "arg_name",
+      },
+      {
+        type: "field_dropdown",
+        name: "operator",
+        options: [
+          [
+            "+",
+            "+",
+          ],
+          [
+            "-",
+            "-"
+          ],
+          [
+            "*",
+            "*"
+          ],
+          [
+            "÷",
+            "/"
+          ]
+        ]
+      },
+      {
+        type: "field_input",
+        name: "num",
+        text: "1"
+      }
+      ],
+      previousStatement: null,
+      nextStatement: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
+
+Blockly.Hat.increment_operator = function (block) {
+  const arg_name = Blockly.Hat.valueToCode(block, 'arg_name', Blockly.Hat.ORDER_FUNCTION_CALL);
+  const operator = block.getFieldValue('operator');
+  const num = block.getFieldValue('num');
+
+  return arg_name + " " + operator + " " + num + " ^(" + arg_name + ")";
+
+};
 
 
 
-  // 比較演算子
-Blockly.Blocks.logic_compare = {
-    init() {
-      this.jsonInit({
-        type: "block_type",
-        message0: "%1 %2 %3 %4",
-        args0: [{
-            type: "input_value",
-            name: "left_side",
-          },
-          {
-            type: "field_dropdown",
-            name: "operator",
-            options: [
-              [
-                "=",
-                "=",
-              ],
-              [
-                "<",
-                "<"
-              ],
-              [
-                ">",
-                ">"
-              ],
-              [
-                "<=",
-                "<="
-              ],
-              [
-                ">=",
-                ">="
-              ]
-            ]
-          },
-          {
-            type: "input_dummy"
-          },
-          {
-            type: "input_value",
-            name: "right_side",
-          }
-        ],
-        output: null,
-        colour: 0,
-        tooltip: "",
-        helpUrl: ""
-      });
-    },
-  };
-  
-  Blockly.Hat.logic_compare = function (block) {
-    const left_side = Blockly.Hat.valueToCode(block, 'left_side', Blockly.Hat.ORDER_FUNCTION_CALL);
-    const operator = block.getFieldValue('operator');
-    const right_side = Blockly.Hat.valueToCode(block, 'right_side', Blockly.Hat.ORDER_FUNCTION_CALL);
-  
-    return left_side + " " + operator + " " + right_side;
-  
-  };
+
+// 変数代入
+Blockly.Blocks.substitution = {
+  init() {
+    this.jsonInit({
+      type: "block_type",
+      message0: "%1 %2 = %3",
+      args0: [{
+        type: "input_value",
+        name: "left_side",
+      },
+      {
+        type: "input_dummy"
+      },
+      {
+        type: "input_value",
+        name: "right_side",
+      }
+      ],
+      previousStatement: null,
+      nextStatement: null,
+      colour: 0,
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+};
+
+Blockly.Hat.substitution = function (block) {
+  const left_side = Blockly.Hat.valueToCode(block, 'left_side', Blockly.Hat.ORDER_FUNCTION_CALL);
+  const right_side = Blockly.Hat.valueToCode(block, 'right_side', Blockly.Hat.ORDER_FUNCTION_CALL);
+
+  return right_side + " ^(" + left_side + ")" + "\n";
+
+};
 
 
 
-  
 
 
 
-  // reduceを定義するブロック
+
+
+
+// reduceを定義するブロック
 Blockly.Blocks.operator = {
   init() {
     this.jsonInit({
       type: "variables_set_number",
       message0: "演算子 %1",
       args0: [{
-          type: "field_dropdown",
-          name: "operator",
-          options: [
-            [
-              "+",
-              "1"
-            ],
-            [
-              "-",
-              "2"
-            ],
-            [
-              "×",
-              "3"
-            ],
-            [
-              "÷",
-              "4"
-            ]
+        type: "field_dropdown",
+        name: "operator",
+        options: [
+          [
+            "+",
+            "1"
+          ],
+          [
+            "-",
+            "2"
+          ],
+          [
+            "×",
+            "3"
+          ],
+          [
+            "÷",
+            "4"
           ]
-        }
+        ]
+      }
       ],
       previousStatement: null,
       nextStatement: null,
@@ -302,33 +335,4 @@ Blockly.Hat.operator = function (block) {
     var operator = "?";
   }
   return operator;
-};
-
-
-
-
-Blockly.Blocks.sample_hoge = {
-  /**
-   * Block for shuffle characters.
-   * @this Blockly.Block
-   */
-  init() {
-    this.jsonInit({
-      type: "print",
-      message0: "sample",
-      args0: [],
-      output: null,
-      colour: 0,
-      tooltip: "",
-      helpUrl: ""
-    });
-  },
-};
-
-Blockly.Hat.sample_hoge = function (block) {
-  var z = "\"(1)\" ^($list_name)"
-  var a = "list_push (1 2) 1 ^($list_name)"
-  var c = "list_push $list_name 2 ^($list_name)"
-  var b = "print( $list_name \"\\n\")^()"
-  return z + "\n\n" + b + "\n\n" + a + "\n\n" + b + "\n\n" + c + "\n\n" + b
 };
